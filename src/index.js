a55bfde5b1d91c648a1e7e488551cc067bd12537
KeyurSpiral <143395296+KeyurSpiral@users.noreply.github.com>
2024-03-14 10:17:36 +0530

    jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj

--- src/index.js
+++ src/index.js
@@ -1,28 +1,16 @@
-// index.js
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then((registration) => {
-        console.log(
-          "Service Worker registered with scope:",
-          registration.scope
-        );
-      })
-      .catch((error) => {
-        console.error("Service Worker registration failed:", error);
-      });
-  });
-}
-
-ReactDOM.render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
+
+reportWebVitals();
