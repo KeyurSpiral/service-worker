61afec74fc145194b01c2fe0768627c5eaa2e11e
KeyurSpiral <143395296+KeyurSpiral@users.noreply.github.com>
2024-03-14 18:54:10 +0530

    yyyyyyyyyyyyyyyyyyyyyyyy

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,7 +1,7 @@
 const register = () => {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
-      navigator.serviceWorker.register("/sw.js").then(
+      navigator.serviceWorker.register("/public/sw.js").then(
         (registration) => {
           console.log(
             "ServiceWorker registration successful with scope: ",
