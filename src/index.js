634fb04fab22a2939cef90fd2cb128d4659942ba
KeyurSpiral <143395296+KeyurSpiral@users.noreply.github.com>
2024-03-14 17:11:45 +0530

    ttttttttttttttttttttttttttttttt

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -11,7 +10,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-serviceWorkerRegistration.register();
 
 reportWebVitals();
