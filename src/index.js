7045cb7a1a62c8a26554e03be7bcb5d57e2c65a9
KeyurSpiral <143395296+KeyurSpiral@users.noreply.github.com>
2024-03-14 17:25:56 +0530

    vvvvvvvvvvvvvvvvvvvvvvvvv

--- src/index.js
+++ src/index.js
@@ -4,18 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then((registration) => {
-        console.log(
-          "Service Worker registered with scope:",
-          registration.scope
-        );
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
       })
-      .catch((error) => {
-        console.error("Service Worker registration failed:", error);
+      .catch(error => {
+        console.error('Service Worker registration failed:', error);
       });
   });
 }
