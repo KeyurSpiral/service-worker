ef9c655e3044fbaefa9ff3016a8be53a983a2d5c
KeyurSpiral <143395296+KeyurSpiral@users.noreply.github.com>
2024-03-15 10:03:06 +0530

    kkkkkkkkkkkkkllllllllllllllllll

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+// import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
@@ -12,6 +12,6 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-serviceWorkerRegistration.register();
+// serviceWorkerRegistration.register();
 
 reportWebVitals();
