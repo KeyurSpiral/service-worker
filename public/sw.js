24b6eb11b9e7664fd34cb2e1be4f63e6f97f6b75
KeyurSpiral <143395296+KeyurSpiral@users.noreply.github.com>
2024-03-15 09:51:32 +0530

    gggggggghhhhhhhhhhhh

--- public/sw.js
+++ public/sw.js
@@ -1,36 +1,5 @@
 // sw.js
 
-const CACHE_NAME = 'counter-app-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/static/js/main.chunk.js',
-  '/static/js/0.chunk.js',
-  '/static/js/bundle.js',
-  'https://jsonplaceholder.typicode.com/posts/1'
-];
-
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
 self.addEventListener('sync', function(event) {
   if (event.tag === 'syncCount') {
     event.waitUntil(syncCount());
@@ -53,16 +22,30 @@ function syncCount() {
     });
 }
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName !== CACHE_NAME;
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/App.css',
+        '/logo.svg',
+        '/app.js',
+        '/index.js',
+        '/serviceWorkerRegistration.js',
+      ]);
     })
   );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
 });
