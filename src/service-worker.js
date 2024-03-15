b0193514b18beea99197f1297cc49fe0ba92f265
KeyurSpiral <143395296+KeyurSpiral@users.noreply.github.com>
2024-03-15 11:20:18 +0530

    AAAAAAAAABBBBBBBB

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,55 +1,56 @@
-import { clientsClaim } from 'workbox-core';
-import { ExpirationPlugin } from 'workbox-expiration';
-import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
-import { registerRoute } from 'workbox-routing';
-import { StaleWhileRevalidate } from 'workbox-strategies';
-import { BackgroundSyncPlugin } from 'workbox-background-sync';
+// src/service-worker.js
+/* eslint-disable */
+import { clientsClaim } from "workbox-core";
+import { ExpirationPlugin } from "workbox-expiration";
+import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
+import { registerRoute } from "workbox-routing";
+import { StaleWhileRevalidate } from "workbox-strategies";
+import { BackgroundSyncPlugin } from 'workbox-background-sync'; // Import BackgroundSyncPlugin
 
 clientsClaim();
 
 precacheAndRoute(self.__WB_MANIFEST);
 
-const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
-registerRoute(
-  ({ request, url }) => {
-    if (request.mode !== 'navigate') {
-      return false;
-    }
+const fileExtensionRegexp = new RegExp("/[^/?]+\\.[^/]+$");
+registerRoute(({ request, url }) => {
+  if (request.mode !== "navigate") {
+    return false;
+  }
 
-    if (url.pathname.startsWith('/_')) {
-      return false;
-    }
+  if (url.pathname.startsWith("/_")) {
+    return false;
+  }
 
-    if (url.pathname.match(fileExtensionRegexp)) {
-      return false;
-    }
+  if (url.pathname.match(fileExtensionRegexp)) {
+    return false;
+  }
 
-    return true;
-  },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
-);
+  return true;
+}, createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html"));
 
 registerRoute(
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'),
+  ({ url }) =>
+    url.origin === self.location.origin && url.pathname.endsWith(".png"),
   new StaleWhileRevalidate({
-    cacheName: 'images',
+    cacheName: "images",
     plugins: [new ExpirationPlugin({ maxEntries: 50 })],
   })
 );
 
+// Register background sync for the desired route
 const bgSyncPlugin = new BackgroundSyncPlugin('myQueueName', {
-  maxRetentionTime: 24 * 60,
+  maxRetentionTime: 24 * 60, // Retry for max of 24 hours (in minutes)
 });
 registerRoute(
-  'https://jsonplaceholder.typicode.com/posts/1',
+  'https://jsonplaceholder.typicode.com/posts/1', // Adjust the URL as per your needs
   new StaleWhileRevalidate({
     plugins: [bgSyncPlugin],
   }),
-  'POST'
+  'POST' // Specify the HTTP method you want to retry
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
